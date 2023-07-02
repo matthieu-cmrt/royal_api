4e127887e0551a25334fda5748b003836894d233
Matthieu Camart <matthieu.camart@epita.fr>
2023-07-03 00:25:42 +0200

    feat(api): base done

---
+++ app.js
@@ -0,0 +1,15 @@
+let express = require("express");
+let app = express();
+let port = 4242;
+app.use(express.json());
+
+app.get("/", (req, res) => {
+res.status(200).json({ message: "Hello World!" });
+console.log("GET /");
+}
+);
+
+app.listen(port, () => {
+console.log(`Server listening on port: ${port}`);
+}
+);
